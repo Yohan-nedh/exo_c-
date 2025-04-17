f16f793102c93724af81963e6d0e2438e8249709
Yohan Nedh <boundadjimadja@gmail.com>
2025-04-17 23:48:16 +0200

    le c++ est cool

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double l, L, P,s, a,b,c,kg;
+    int N[5], som = 0, taux;
+    /*
+     //Notions de base
+    // excercice 1
+    cout << "Hello World!" << endl;
+    cout << "Voici un programme illustrant l'utilisation de cout!"<< endl;
+
+
+    // excercice 2
+    cout << "Veuillez entrer la largeur du champ: ";
+    cin >> l ;
+    cout << " Veuillez entrer la longueur du champ: ";
+    cin >> L ;
+    P = 2*(l+L);
+    cout << " Voici le perimetre du champ: " << P << "cm²";
+    s = l*L;
+    cout << "Voici la surface du champ: " << s << "cm²"  << endl;
+
+    // excercice 3
+    for (int a = 0; a < 5; a++) {
+        cout << "Veuillez entrer cinq entiers "<< a +1 << ":";
+        cin >> N[a];
+        som += N[a];
+    }
+    som /= 5;
+    cout << "Voici la moyenne: "<< som << endl;
+
+
+    // excercice 4
+    cout << "Veuillez entrer un entier A: " ;
+    cin >> a;
+    cout << "Veuillez entrer un entier B: " ;
+    cin >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << "voici la new valeur  de A: " << a << endl;
+    cout << "voici la new valeur  de B: " << b << endl;
+
+
+    // excercice 5
+    cout<< "Calcul de prix TTC de marchandise."<< endl;
+    cout << "Le prix d'un kilo de tomate est à 500FCFA"<< endl;
+    cout << "Combien de kilo de tomate vous voulez: ";
+    cin >> kg;
+    cout << "Veuillez entrer la TVA:";
+    cin >> taux;
+    int ht = 500 * kg;
+    cout << "Voici le prix total HT: " << ht << "FCFA\n";
+    int tva = (500 * taux)/100;
+    cout << "le taux de tva est: " <<tva << "FCFA";
+
+
+    //Structures de contrôles
+     int num;
+    // excercice 1
+    cout << "Enter a number that is not too far from 80 but not lower than 50: ";
+    cin >> num;
+    if (num > 60 && num < 75) {
+        cout << "You win";
+    }
+    else {
+        cout << "You lose";
+    }
+    */
+
+    // excercice 2
+
+
+
+
+    return 0;
+}
+
